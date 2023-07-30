3da6f6cacb026655e762cb6dd5638256f5f40ea0
Ummed Choudhary <ummedc184@gmail.com>
2023-07-30 07:17:22 +0530

    commit all

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "./../../assets/logo.png";
+import Logo from "./../../assets/newclub.png";
 import "./Header.css";
 import bars from "./../../assets/bars.png";
 import { useState } from "react";
